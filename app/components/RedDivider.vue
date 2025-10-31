14d190c32dda9a1555da5cc07f3a2ef5ff12c8c7
Karolina Krysiak <71433414+KarolinaKrysiak@users.noreply.github.com>
2025-10-31 14:44:51 +0100

    main page 1st section + draft of 2nd

---
+++ app/components/RedDivider.vue
@@ -0,0 +1,9 @@
+<template>
+	<div class="flex flex-col items-center mb-12 z-10">
+		<div class="flex flex-row items-center">
+			<span class="h-1.5 w-[100px] block bg-custom-red rounded-full m-2.5"></span>
+			<span class="h-1.5 w-[100px] block bg-custom-red rounded-full m-2.5"></span>
+		</div>
+		<span class="h-1.5 w-[100px] block bg-custom-red rounded-full m-2.5"></span>
+	</div>
+</template>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,6 +1,37 @@
 <template>
 	<div>
-		<h2 class="text-6xl text-custom-blue mb-12 text-center">My Portfolio</h2>
-		<p class="max-w-2xl px-2.5 my-5 mx-auto text-lg">Welcome to my portfolio website.</p>
+		<section class="h-screen relative">
+			<div class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 z-10">
+				<h1>Karolina Krysiak</h1>
+				<h2 class="font-sans">Software Engineer</h2>
+			</div>
+			<img
+				alt="background circle"
+				src="assets/img/circle.png"
+				class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2"
+			/>
+		</section>
+		<section class="h-[600px] flex items-center overflow-y-hidden">
+			<div class="flex flex-col w-1/2">
+				<img
+					alt="painted over picture of me"
+					src="assets/img/pic.png"
+					class="min-w-full min-h-[600px] object-cover"
+				/>
+			</div>
+			<div class="flex flex-col w-1/2">
+				<div>
+					<h3>A bit about me</h3>
+					<RedDivider />
+
+					<p>
+						I am a 5th semester student at VIA University Horsens on a Software Technology Engineering programme.
+						I also have an AP Degree in Multimedia Design and Communication with frontend development
+						specialization from Business Academy Aarhus.
+					</p>
+					<NuxtLink to="/about">Read more about me</NuxtLink>
+				</div>
+			</div>
+		</section>
 	</div>
 </template>
