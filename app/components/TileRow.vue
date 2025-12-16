ebf945382ea8966138151e0b06e87b19f602cc4b
Karolina Krysiak <71433414+KarolinaKrysiak@users.noreply.github.com>
2025-12-16 11:14:48 +0100

    fix: tile row into component

---
+++ app/components/TileRow.vue
@@ -0,0 +1,21 @@
+<template>
+	<div class="h-[600px] flex flex-row items-center overflow-y-hidden" :class="reverse ? 'flex-row-reverse' : ''">
+		<div class="flex flex-col w-1/2">
+			<slot name="image" />
+		</div>
+		<div class="flex flex-col w-1/2">
+			<div>
+				<slot name="content" />
+			</div>
+		</div>
+	</div>
+</template>
+
+<script setup>
+const props = defineProps({
+	reverse: {
+		type: Boolean,
+		default: false,
+	},
+});
+</script>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -11,41 +11,39 @@
 				class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2"
 			/>
 		</section>
-		<section class="h-[600px] flex items-center overflow-y-hidden">
-			<div class="flex flex-col w-1/2">
-				<img
-					alt="painted over picture of me"
-					src="assets/img/pic.png"
-					class="min-w-full min-h-[600px] object-cover"
-				/>
-			</div>
-			<div class="flex flex-col w-1/2">
-				<div>
+
+		<section id="tile-rows">
+			<TileRow>
+				<template #image>
+					<img
+						alt="painted over picture of me"
+						src="assets/img/pic.png"
+						class="min-w-full min-h-[600px] object-cover"
+					/>
+				</template>
+				<template #content>
 					<h3>A bit about me</h3>
 					<RedDivider />
-
 					<p>
 						I am a 5th semester student at VIA University Horsens on a Software Technology Engineering programme.
 						I also have an AP Degree in Multimedia Design and Communication with frontend development
 						specialization from Business Academy Aarhus.
 					</p>
 					<PurpleButton to="/about">Read more</PurpleButton>
-				</div>
-			</div>
-		</section>
-		<section class="h-[600px] flex items-center overflow-y-hidden">
-			<div class="flex flex-col w-1/2">
-				<div>
+				</template>
+			</TileRow>
+
+			<TileRow reverse>
+				<template #image>
+					<img alt="" src="" class="min-w-full min-h-[600px] object-cover" />
+				</template>
+				<template #content>
 					<h3>The projects I have worked on</h3>
 					<RedDivider />
-
 					<p>*short description of types of projects in general*</p>
 					<PurpleButton to="/about">See more</PurpleButton>
-				</div>
-			</div>
-			<div class="flex flex-col w-1/2">
-				<img alt="" src="" class="min-w-full min-h-[600px] object-cover" />
-			</div>
+				</template>
+			</TileRow>
 		</section>
 	</div>
 </template>
